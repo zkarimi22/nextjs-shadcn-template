d800214a66d774e1001a774dcb44dfdc1e1b600d
Zalmy K <60475531+zkarimi22@users.noreply.github.com>
2025-10-25 11:18:03 -0400

    updated repos

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
